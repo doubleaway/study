f88165da5bd514f35ff8aa50351feb8d13df6da0
DESKTOP-92SISGK\yangcharin <ycl042138@gmail.com>
2021-11-05 13:50:16 +0900

    항목 추가  - spread연산자 사용 or concat 함수 사용  * push,splice sort는 사용하면 안됨. #꼭 사용해야한다면 복사하여 사용

--- input/src/App.js
+++ input/src/App.js
@@ -1,19 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 import UserList from "./userList";
-import {useRef} from "react";
+import {useRef, useState} from "react";
+import CreateUser from "./createUser";
 //고유 아이디값을 관리하기 위해 useref사용
 function App() {
   const nextId=useRef(4);
+  const [inputs,setInputs]=useState({
+    username:'',
+    email:'',
+  });
+  const {username,email}=inputs;
+  const onChange=e=>{
+    const {name,value}=e.target;
+    setInputs({
+      ...inputs,
+      [name]:value
+    })
+  }
 
   //항목을 추가 할 시
   const onCreate=()=>{
+    const user={
+      id:nextId.current,
+      username,
+      email,
+    };
+
+
+    //push,splice sort는 사용하면 안됨. #꼭 사용해야한다면 복사하여 사용
+    //spread연산자 사용 방법
+      // setUsers([...users,user]);
+    //concat 함수 사용 방법
+    setUsers(users.concat(user));
+
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+
+
+
     console.log(nextId.current);//4
     //함수 호출할때마다 1씩 더해주기.
     nextId.current+=1;
   }
 
-  const users=[
+
+
+
+  const [users,setUsers]=useState([
     {
       id:1,
       username:'veloper',
@@ -29,9 +66,17 @@ function App() {
       username:'simon',
       email:'qlqp;oprewjf@naver.com'
     },
-  ];
+  ]);
   return (
-   <UserList users={users}/>
+      <>
+        <CreateUser
+            username={username}
+            email={email}
+            onChange={onChange}
+            onCreate={onCreate}
+        />
+        <UserList users={users}/>
+        </>
   );
 }
 
